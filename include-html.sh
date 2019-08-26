a7ce16fd5c76f2b2f47f5fc5d09086427d991fb5
Panupong <5635512110@psu.ac.th>
2019-08-27 05:08:05 +0700

    update

---
+++ include-html.sh
@@ -0,0 +1,13 @@
+echo "
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <title>vue-cli</title>
+  </head>
+  <body>
+    <div id="app"></div>
+    <script src="/build.js"></script>
+  </body>
+</html>
+" > dist/index.html
